bbb49a97609644c30d8c5251d1b204131c9bc992
eunmmm <rosiebaek223@gmail.com>
2024-07-14 02:58:38 +0900

    Setting: [T-01] 초기 프로젝트 세팅 완료
    - Electron Forge를 사용하여 프로젝트 설정
    - Vite를 사용한 React 설정
    - ESLint 및 Prettier 설정
    - Husky 및 lint-staged 설정
    
    https://eunmi.notion.site/T-01-f6417efaf5e240bfac91c1c2a3cfd9a1?pvs=4

---
+++ README.md
@@ -0,0 +1,1 @@
+# Eclectic-editor-app
---
+++ src/main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+  });
+
+  if (process.env.MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(process.env.MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/renderer/index.html'));
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
