25c6bff55e99e80307010f92c3a8051256863bdc
eunmmm <rosiebaek223@gmail.com>
2024-07-24 20:46:23 +0900

    Add: [T-09] 배경 관련 상수 background.js 파일로 분리
    https://www.notion.so/eunmi/T-09-Background-3b5449c965cc4446ab5c41f75db29f34?pvs=4

---
+++ src/renderer/constants/background.js
@@ -0,0 +1,23 @@
+export const BACKGROUND_REPEAT_OPTIONS = [
+  'repeat',
+  'repeat-x',
+  'repeat-y',
+  'space',
+  'round',
+  'no-repeat',
+];
+
+export const BACKGROUND_ATTACHMENT_OPTIONS = ['scroll', 'fixed', 'local'];
+
+export const BACKGROUND_ORIGIN_OPTIONS = [
+  'padding-box',
+  'border-box',
+  'content-box',
+];
+
+export const BACKGROUND_CLIP_OPTIONS = [
+  'border-box',
+  'padding-box',
+  'content-box',
+  'text',
+];
