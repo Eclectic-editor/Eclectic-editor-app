64e34eb846561d54219b48216f63ed5df273b2e2
eunmmm <rosiebaek223@gmail.com>
2024-07-24 17:52:13 +0900

    Feat: [T-09] EditorImageInput 컴포넌트 생성
    - 이미지 URL 입력 및 파일 선택 기능을 위한 컴포넌트 생성
    - 파일 선택 시 파일 이름 표시 기능 구현
    
    https://www.notion.so/eunmi/T-09-Background-3b5449c965cc4446ab5c41f75db29f34?pvs=4

---
+++ src/renderer/components/EditorImageInput/index.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+
+import EditorInput from '../EditorInput';
+
+import './style.scss';
+
+function EditorImageInput({ id, label, value, onImageChange }) {
+  const [inputValue, setInputValue] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  useEffect(() => {
+    if (value && value.startsWith('data:')) {
+      setInputValue(fileName);
+    } else {
+      setInputValue(value || '');
+    }
+  }, [value, fileName]);
+
+  const handleFileChange = (e) => {
+    const [file] = e.target.files;
+
+    if (file) {
+      setFileName(file.name);
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const { result } = reader;
+        onImageChange(result);
+        setInputValue(file.name);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleInputChange = (newValue) => {
+    setInputValue(newValue);
+
+    if (newValue === '') {
+      setFileName('');
+      onImageChange(null);
+    } else if (!newValue.startsWith('data:')) {
+      setFileName('');
+      onImageChange(newValue);
+    }
+  };
+
+  return (
+    <div className="editor-image-input">
+      <label htmlFor={`${id}-file`} className="editor-title">
+        {label}
+      </label>
+      <div className="input-file-box">
+        <input
+          type="file"
+          id={`${id}-file`}
+          className="image-file-input"
+          onChange={handleFileChange}
+          accept="image/*"
+        />
+        <label htmlFor={`${id}-file`} className="file-name">
+          Select a File
+        </label>
+      </div>
+      <strong className="box-file-title">Image Source</strong>
+      <EditorInput
+        id={`${id}-url`}
+        label="Background Image"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter image URL or select a file"
+      />
+    </div>
+  );
+}
+
+export default EditorImageInput;
