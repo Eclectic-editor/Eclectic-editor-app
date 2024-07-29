faeb64ce1bf167297d4899bfc4a6704a38e68aea
eunmmm <rosiebaek223@gmail.com>
2024-07-29 19:59:51 +0900

    Refactor: [T-14] Border 관련 상수들을 정리하여 별도의 파일로 분리

---
+++ src/renderer/constants/border.js
@@ -0,0 +1,58 @@
+export const BORDER_AREA = 'border';
+
+export const BORDER_OPTIONS = ['all', 'top', 'right', 'bottom', 'left'];
+
+export const BORDER_STYLE = ['none', 'solid', 'dashed', 'dotted'];
+
+export const BORDER_RADIUS_PROPERTIES = {
+  Top: 'borderTopLeftRadius',
+  Right: 'borderTopRightRadius',
+  Bottom: 'borderBottomRightRadius',
+  Left: 'borderBottomLeftRadius',
+};
+
+export const BORDER_PROPERTIES = {
+  borderWidth: [
+    'borderTopWidth',
+    'borderRightWidth',
+    'borderBottomWidth',
+    'borderLeftWidth',
+  ],
+  borderColor: [
+    'borderTopColor',
+    'borderRightColor',
+    'borderBottomColor',
+    'borderLeftColor',
+  ],
+  borderStyle: [
+    'borderTopStyle',
+    'borderRightStyle',
+    'borderBottomStyle',
+    'borderLeftStyle',
+  ],
+  borderRadius: [
+    'borderTopLeftRadius',
+    'borderTopRightRadius',
+    'borderBottomRightRadius',
+    'borderBottomLeftRadius',
+  ],
+};
+
+export const ALL_BORDER_PROPERTIES = [
+  'borderTopWidth',
+  'borderRightWidth',
+  'borderBottomWidth',
+  'borderLeftWidth',
+  'borderTopColor',
+  'borderRightColor',
+  'borderBottomColor',
+  'borderLeftColor',
+  'borderTopStyle',
+  'borderRightStyle',
+  'borderBottomStyle',
+  'borderLeftStyle',
+  'borderTopLeftRadius',
+  'borderTopRightRadius',
+  'borderBottomRightRadius',
+  'borderBottomLeftRadius',
+];
